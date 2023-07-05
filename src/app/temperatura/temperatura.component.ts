89c6b1c42b06e477cd660bc55326c86714f57658
Lucbm99 <lbaradelmarchiori@gmail.com>
2023-07-04 21:58:44 -0300

    Ajustes

--- src/app/temperatura/temperatura.component.ts
+++ src/app/temperatura/temperatura.component.ts
@@ -17,7 +17,6 @@ export class TemperaturaComponent implements OnInit {
   public valorUmidadeCidade: any;
   public textoCondicao: string = '';
   public imagemTempo: string = '';
-  public inputValue: string = '';
 
   public temperaturaForm: any;
   public cidade: string = '';
@@ -36,37 +35,39 @@ export class TemperaturaComponent implements OnInit {
 
   pesquisarTemperaturaDaCidadeFornecida() {
     this._temperaturaService.pesquisarTemperaturaDeUmaCidade(this.temperaturaForm.value.cidade).subscribe((response) => {
-      console.log('Entered value:', this.inputValue);
+      const nameValue = this.temperaturaForm.get('cidade').value;
+      console.log('Entered value:', nameValue);
 
       if (this.temperaturaForm.get('cidade').value === null) {
         alert("Campo em branco. Verifique e tente novamente.");
         return;
+      } else {
+
+        const data = [ response ];
+        this.formularioSubmetido = true;
+
+        const dadosExtraidosAPI = data.map(item => {
+          const localizacao = item.location.name;
+          const regiao = item.location.region;
+          const pais = item.location.country;
+          const temperaturaCelsius = item.current.temp_c;
+          const temperaturaFahrenheit = item.current.temp_f;
+          const temperaturaKelvin = temperaturaCelsius + 273.15;
+          const umidade = item.current.humidity;
+          const condicaoTemperatura = item.current.condition.text;
+          const imagemTemperatura = item.current.condition.icon;
+
+          this.localizacao = localizacao;
+          this.regiao = regiao;
+          this.pais = pais;
+          this.valorTemperaturaCidadeCelsius = temperaturaCelsius;
+          this.valorTemperaturaCidadeKelvin = temperaturaKelvin;
+          this.valorTemperaturaCidadeFahrenheit = temperaturaFahrenheit;
+          this.valorUmidadeCidade = umidade;
+          this.textoCondicao = condicaoTemperatura;
+          this.imagemTempo = imagemTemperatura;
+        });
       }
-
-      const data = [ response ];
-      this.formularioSubmetido = true;
-
-      const dadosExtraidosAPI = data.map(item => {
-        const localizacao = item.location.name;
-        const regiao = item.location.region;
-        const pais = item.location.country;
-        const temperaturaCelsius = item.current.temp_c;
-        const temperaturaFahrenheit = item.current.temp_f;
-        const temperaturaKelvin = temperaturaCelsius + 273.15;
-        const umidade = item.current.humidity;
-        const condicaoTemperatura = item.current.condition.text;
-        const imagemTemperatura = item.current.condition.icon;
-
-        this.localizacao = localizacao;
-        this.regiao = regiao;
-        this.pais = pais;
-        this.valorTemperaturaCidadeCelsius = temperaturaCelsius;
-        this.valorTemperaturaCidadeKelvin = temperaturaKelvin;
-        this.valorTemperaturaCidadeFahrenheit = temperaturaFahrenheit;
-        this.valorUmidadeCidade = umidade;
-        this.textoCondicao = condicaoTemperatura;
-        this.imagemTempo = imagemTemperatura;
-      });
     })
   }
 
